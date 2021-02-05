b32296a9f895bfb50b78b8189c16f93813ec6a7c
Gourab karmakar <gourabkarmakar297@gmail.com>
2021-02-05 16:11:16 +0530

    Add some Data In hello.py and 500.html

--- hello.py
+++ hello.py
@@ -27,7 +27,7 @@ def user(name):
 def page_not_found(Error):
 	return render_template("404.html"),404
 
-#Internal Server Error
+# Internal Server Error
 @app.errorhandler(500)
 def page_not_found(Error):
 	return render_template("500.html"),500
