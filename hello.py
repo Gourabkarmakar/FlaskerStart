36c6ad842462cb5ba48d07f993ff25c8a93e113f
Gourab karmakar <gourabkarmakar297@gmail.com>
2021-05-15 16:05:20 +0530

    UPDATE PROGRAM : Add werkzeug for authentication

--- hello.py
+++ hello.py
@@ -7,6 +7,8 @@ from datetime import datetime
 # from text import user_name,password
 import text
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # Create a Flask Instance
 app = Flask(__name__)
@@ -29,6 +31,21 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.now)
+
+    # Password section
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password Is Not a readable attribuite')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
     # Create a String
     def __repr__(self):
